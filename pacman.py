4a6a10a5a1562948d7d252fbd8a5c348773b8e00
Remigiusz Najbar <muffespl@gmail.com>
2020-03-08 14:41:45 +0100

    Added .gitignore

--- pacman.py
+++ pacman.py
@@ -1,7 +1,8 @@
 import pygame
+
 pygame.init()
 
-#original pacman resolution - 224x228 with 8x8px tiles (28x36)
+# original pacman resolution - 224x228 with 8x8px tiles (28x36)
 pygame.display.set_mode((224, 288))
 pygame.display.set_caption("Pacman")
 
@@ -44,18 +45,11 @@ for y, line_str in enumerate(game_map_str.splitlines()):
     for x, cell in enumerate(line_str):
         print(x, y - 1, cell)
 
-
 tick_count = 60
 base_delay = 1000 / tick_count
 
 while True:
     start_time = pygame.time.get_ticks()
-
-
-
-
-
-
 
     end_time = pygame.time.get_ticks()
     pygame.time.delay(int(base_delay - end_time + start_time))
